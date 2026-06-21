1aa00feaf4b8d985eb7c1d3202b9215508effea6
its-saeed <its-saeed@users.noreply.github.com>
2026-06-21 07:50:29 +0000

    deploy: 630efb065e40914bf49661205b4dac30e108d2c7

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-27bbe08e.js',
+                path_to_root + 'searchindex-f5fe6ab5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
